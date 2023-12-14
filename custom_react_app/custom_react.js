29bb852866454bcf80103043ba14da3bc6e4a0be
najmulhouda <najmulhouda6@gmail.com>
2023-12-14 23:42:04 +0600

    custom react app

--- custom_react_app/custom_react.js
+++ custom_react_app/custom_react.js
@@ -1,1 +1,11 @@
+const reactElement = {
+    type : 'a',
+    props:{
+        href: 'https://reactjs.org/',
+        target: '_blank',
+    },
+    children: 'React',
+}
+
+
 const mainContainer = document.querySelector('#root');
