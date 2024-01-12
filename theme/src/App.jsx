600fe9a5319c40d7a065f4081f0b110232a0f2cc
najmulhouda <najmulhouda6@gmail.com>
2024-01-12 16:18:50 +0600

    project completed

--- theme/src/App.jsx
+++ theme/src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import Card from "./components/Card";
+import ThemeBtn from "./components/ThemeBtn";
 import { ThemeProvider } from "./contexts/Theme";
 
 function App() {
@@ -16,14 +18,16 @@ function App() {
   }, [themeMode]);
   return (
     <>
-      <ThemeProvider value={{ themeMode, lighTheme, darkTheme }}>
+      <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
         <div className="flex flex-wrap min-h-screen items-center">
           <div className="w-full">
             <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-              {/* THEME */}
+              <ThemeBtn />
             </div>
 
-            <div className="w-full max-w-sm mx-auto">{/* CARD */}</div>
+            <div className="w-full max-w-sm mx-auto">
+              <Card />
+            </div>
           </div>
         </div>
       </ThemeProvider>
