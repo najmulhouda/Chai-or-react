bfff751c8e847cdbf6a99263e4dcd16c0cf84bb8
najmulhouda <najmulhouda6@gmail.com>
2024-01-14 12:49:51 +0600

    1

--- todoContext/src/Component/TodoItem.jsx
+++ todoContext/src/Component/TodoItem.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useTodo } from "../contexts";
 
 function TodoItem({ todo }) {
+  const [isTodoEditable, setIsTodoEditable] = React.useState(false);
+  const [todoMsg, setTodoMsg] = React.useState(todo.todo);
+  const { updatedTodo, deleteTodo, toggleCompleted } = useTodo();
   return (
     <div
       className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
