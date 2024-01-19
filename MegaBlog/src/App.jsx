348f80cefd0890aa90c6e58831afc02423b10f77
najmulhouda <najmulhouda6@gmail.com>
2024-01-19 23:27:33 +0600

    update

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -7,9 +7,6 @@ function App() {
   return (
     <>
       <h1>Mega Blog Project</h1>
-      <h1>Mega Blog Project</h1>
-      <h1>Mega Blog Project</h1>
-      <h1>Mega Blog Project</h1>
     </>
   );
 }
