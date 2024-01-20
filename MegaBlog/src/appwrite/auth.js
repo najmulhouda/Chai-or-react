0a9aaf746349a2ba5f328a300ca21ea3ae00aed6
najmulhouda <najmulhouda6@gmail.com>
2024-01-20 10:46:33 +0600

    create account and loging part completed

--- MegaBlog/src/appwrite/auth.js
+++ MegaBlog/src/appwrite/auth.js
@@ -1,4 +1,4 @@
-import { Account, Client } from "appwrite";
+import { Account, Client, Id } from "appwrite";
 import config from "../config/config";
 
 
@@ -11,12 +11,31 @@ export class AuthService  {
             .setProject(config.appWriteProjectId); // Your project ID
         this.account = new Account(this.client);
     }
+    async createAccount(email, password , name) {
+        try {   
+            const useAccount = await this.account.create(Id.unique(), email, password, name);
+            if (useAccount) {
+                //call another method
+                return this.login(email, password)
+            } else {
+                return useAccount
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+    async login(email, password) {
+        try {
+            return await this.account.createEmailSession(email, password);
+           
+        } catch (error) {
+            throw error
+        }
+    }
+
+
 }
 
 const authService = new AuthService()
 
 export default AuthService
-export default AuthService
-export default AuthService
-
-
