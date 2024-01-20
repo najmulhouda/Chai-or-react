caae29a89c1bf7e0c131128039e652898a34ee67
najmulhouda <najmulhouda6@gmail.com>
2024-01-20 23:37:46 +0600

    authentiket userData

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,9 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 
 function App() {
-  const [count, setCount] = useState(0);
-  console.log();
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((user) => {
+        if (user) {
+          dispatch(login(user));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <>
       <h1>Mega Blog Project</h1>
