e9337d4f889ed2c1af9ff6d5efaf5b5125abe3ff
najmulhouda <najmulhouda6@gmail.com>
2024-01-21 23:50:44 +0600

    heading part completed

--- MegaBlog/src/components/Header/Header.jsx
+++ MegaBlog/src/components/Header/Header.jsx
@@ -47,9 +47,19 @@ function Header() {
             {navItems.map((item) =>
               item.active ? (
                 <li key={item.slug}>
-                  <Link to={item.slug}>{item.name}</Link>
+                  <button
+                    onClick={() => navitage(item.slug)}
+                    className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                  >
+                    {item.name}
+                  </button>
                 </li>
               ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
             )}
           </ul>
         </nav>
