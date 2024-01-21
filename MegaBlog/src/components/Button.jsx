f93a78b571800d8c856fef2a1accc717c96006a2
najmulhouda <najmulhouda6@gmail.com>
2024-01-21 23:55:41 +0600

    default button

---
+++ MegaBlog/src/components/Button.jsx
@@ -0,0 +1,18 @@
+import React, { Children } from 'react'
+
+function Button() {
+    Children,
+    type = "button",
+    bgColor = "bg-blue-600",
+    textColor = "text-white",
+    className = "",
+    ...props 
+
+  return (
+    <button>
+        {Children}
+    </button>
+  )
+}
+
+export default Button
