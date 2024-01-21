26e636786837a7d28a3aa2c509a850429e2f52f3
najmulhouda <najmulhouda6@gmail.com>
2024-01-22 00:53:29 +0600

    input field completed

--- MegaBlog/src/components/Button.jsx
+++ MegaBlog/src/components/Button.jsx
@@ -1,19 +1,21 @@
-import React, { Children } from 'react'
+import React from "react";
 
-function Button() {
-    Children,
-    type = "button",
-    bgColor = "bg-blue-600",
-    textColor = "text-white",
-    className = "",
-    ...props
-    
-
+function Button({
+  Children,
+  type = "button",
+  bgColor = "bg-blue-600",
+  textColor = "text-white",
+  className = "",
+  ...props
+}) {
   return (
-    <button className={`${bgColor} ${textColor} ${className}`} {...props}>
-        {Children}
+    <button
+      className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`}
+      {...props}
+    >
+      {Children}
     </button>
-  )
+  );
 }
 
-export default Button
+export default Button;
