c1430ff98cd20c272deeefdbaeac4ad5e858a673
najmulhouda <najmulhouda6@gmail.com>
2024-01-21 23:17:19 +0600

    header page

--- MegaBlog/src/components/Header/Header.jsx
+++ MegaBlog/src/components/Header/Header.jsx
@@ -1,7 +1,48 @@
 import React from "react";
-
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { Container } from "../index";
 function Header() {
-  return <div>Header</div>;
+  const authStatus = useSelector((state) => state.auth.status);
+  const navitage = useNavigate();
+
+  const navItems = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true,
+    },
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+    },
+  ];
+
+  return (
+    <header>
+      <Container>
+        <nav className="flex items-center justify-between h-16">
+          <div className="mr-4"></div>
+        </nav>
+      </Container>
+    </header>
+  );
 }
 
 export default Header;
