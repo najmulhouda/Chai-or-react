c71039025646eda0e0f764c77915ed7c67e2fc7d
najmulhouda <najmulhouda6@gmail.com>
2024-01-22 23:57:57 +0600

    regex

--- MegaBlog/src/components/Login.jsx
+++ MegaBlog/src/components/Login.jsx
@@ -57,7 +57,16 @@ function Login() {
                 label="Email"
                 placeholder="Enter your email"
                 type="email"
-                {...register("email"), { required: true }}
+                {...register("email"), { 
+                    required: true,
+                    validate:{
+                        matchPatern: (value) => {
+                            return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+                                value
+                            );
+                        }
+                    }
+                }}
 
                 
                 />
