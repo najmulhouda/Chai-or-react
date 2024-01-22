2c8736b75c1e7896490268fef6bc666a18d1d43f
najmulhouda <najmulhouda6@gmail.com>
2024-01-22 13:09:56 +0600

    working on login component

---
+++ MegaBlog/src/components/Login.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import authService from "../appwrite/auth";
+import { login as authLogin } from "../store/authSlice";
+
+function Login() {
+  const navigation = useNavigate();
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
+  const [error, setError] = useState("");
+
+  const login = async (data) => {
+    setError("");
+    try {
+      const session = await authService.login(data);
+      if (session) {
+        const userData = await authService.getCurrentUser();
+        if (userData) dispatch(authLogin(userData));
+        navigation("/");
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return <div>Login</div>;
+}
+
+export default Login;
--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -1,7 +1,9 @@
+import Button from "./Button";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
+import Select from "./Select";
 import Container from "./container/Container";
-export { Container, Footer, Header, Logo, LogoutBtn };
+export { Button, Container, Footer, Header, Logo, LogoutBtn, Select };
 
