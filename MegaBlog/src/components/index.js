54fedd9dd6f4d30a38758f68300c8fa93a1edeea
najmulhouda <najmulhouda6@gmail.com>
2024-01-23 12:57:50 +0600

    postForm comepleted

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -3,7 +3,10 @@ import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
+import RTE from "./RTE";
 import Select from "./Select";
 import Container from "./container/Container";
-export { Button, Container, Footer, Header, Logo, LogoutBtn, Select };
+
+
+export { Button, Container, Footer, Header, Logo, LogoutBtn, RTE, Select };
 
