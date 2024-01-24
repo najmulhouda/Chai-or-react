e182916b3dc318b0506198ce53e775225b5a3c66
najmulhouda <najmulhouda6@gmail.com>
2024-01-24 23:33:28 +0600

    router setup done:

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -1,17 +1,18 @@
+import AuthLayout from "./AuthLayout";
 import Button from "./Button";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
 import Login from "./Login";
 import Logo from "./Logo";
-import Post from "./Post/Post";
 import PostCard from "./PostCard";
 import RTE from "./RTE";
 import Select from "./Select";
 import Signup from "./Signup";
 import Container from "./container/Container";
-import { PostForm } from "./post-form/PostForm";
-
-
-export { Button, Container, Footer, Header, Login, Logo, LogoutBtn, Post, PostCard, PostForm, RTE, Select, Signup };
-
+import PostForm from "./post-form/PostForm";
+export {
+    AuthLayout, Button, Container, Footer, Header, Input, Login, Logo,
+    LogoutBtn, PostCard, PostForm, RTE, Select, Signup
+};
