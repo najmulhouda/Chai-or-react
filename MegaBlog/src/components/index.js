45b39032a7982ca5427172e6729e118835f428fc
najmulhouda <najmulhouda6@gmail.com>
2024-01-24 12:47:44 +0600

    Login page completed

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -2,11 +2,15 @@ import Button from "./Button";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import LogoutBtn from "./Header/LogoutBtn";
+import Login from "./Login";
 import Logo from "./Logo";
+import PostCard from "./PostCard";
 import RTE from "./RTE";
 import Select from "./Select";
+import Signup from "./Signup";
 import Container from "./container/Container";
+import { PostForm } from "./post-form/PostForm";
 
 
-export { Button, Container, Footer, Header, Logo, LogoutBtn, RTE, Select };
+export { Button, Container, Footer, Header, Login, Logo, LogoutBtn, PostCard, PostForm, RTE, Select, Signup };
 
